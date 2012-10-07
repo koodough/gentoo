5cdcda5e4443963f0fc424d5bf3b8792fd371475
Robin J. Rogge <rojaro@gmail.com>
2012-10-07 16:52:42 +0200

    updated generate_make_conf.rb

--- definitions/generate_make_conf.rb
+++ definitions/generate_make_conf.rb
@@ -1,6 +1,6 @@
 define :generate_make_conf do
-  Chef::Log.debug("Generating /etc/make.conf: #{params[:name]}")
-  template "/etc/make.conf" do
+  Chef::Log.debug("Generating /etc/portage/make.conf: #{params['name']}")
+  template "/etc/portage/make.conf" do
     source "make.conf.erb"
     cookbook "gentoo"
     owner "root"
@@ -8,22 +8,30 @@ define :generate_make_conf do
     mode "0644"
     action :create
     variables(
-      :use_flags => node[:gentoo][:use_flags],
-      :chost => "#{node[:kernel][:machine]}-pc-linux-gnu",
-      :cflags => node[:gentoo][:cflags],
-      :makeopts => [node[:gentoo][:makeopts]].flatten,
-      :features => [node[:gentoo][:portage_features]].flatten,
-      :emerge_options => [node[:gentoo][:emerge_options]].flatten,
-      :overlays => [node[:gentoo][:overlay_directories]].flatten,
-      :collision_ignores => [node[:gentoo][:collision_ignores]].flatten,
-      :licenses => [node[:gentoo][:accept_licenses]].flatten,
-      :ruby_targets => [node[:gentoo][:ruby_targets]].flatten,
-      :use_expands => node[:gentoo][:use_expands],
-      :elog_mailuri => node[:gentoo][:elog_mailuri],
-      :elog_mailfrom => node[:gentoo][:elog_mailfrom],
-      :rsync_mirror => node[:gentoo][:rsync_mirror],
-      :distfile_mirrors => [node[:gentoo][:distfile_mirrors]].flatten,
-      :portage_binhost => node[:gentoo][:portage_binhost]
+      :cflags => node['gentoo']['cflags'],
+      :chost => "#{node['kernel']['machine']}-pc-linux-gnu",
+      :cssflags => node['gentoo']['cxxflags'],
+      :distdir => node['gentoo']['distdir'],
+      :distfile_mirrors => [node['gentoo']['distfile_mirrors']].flatten,
+      :ebeep_ignore => node['gentoo']['ebeep_ignore'],
+      :elog_classes => node['gentoo']['elog_classes'],
+      :elog_mailfrom => node['gentoo']['elog_mailfrom'],
+      :elog_mailsubject => node['gentoo']['elog_mailsubject'],
+      :elog_mailuri => node['gentoo']['elog_mailuri'],
+      :emerge_options => [node['gentoo']['emerge_options']].flatten,
+      :features => [node['gentoo']['portage_features']].flatten,
+      :licenses => [node['gentoo']['accept_licenses']].flatten,
+      :makeopts => [node['gentoo']['makeopts']].flatten,
+      :overlays => [node['gentoo']['overlay_directories']].flatten,
+      :pkgdir => node['gentoo']['pkgdir'],
+      :port_logdir => node['gentoo']['port_logdir'],
+      :portage_binhost => node['gentoo']['portage_binhost'],
+      :portage_niceness => node['gentoo']['portage_niceness'],
+      :portdir => node['gentoo']['portdir'],
+      :rsync_mirror => node['gentoo']['rsync_mirror'],
+      :ruby_targets => [node['gentoo']['ruby_targets']].flatten,
+      :use_expands => node['gentoo']['use_expands'],
+      :use_flags => node['gentoo']['use_flags']
     )
   end
 end
